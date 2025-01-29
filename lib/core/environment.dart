66c9735f121ec122e861a560ad29cc233743f3fb
Oleksii Tatarintsev <semaxdev@gmail.com>
2025-01-29 21:04:25 +0200

    initial project

---
+++ lib/core/environment.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class Environment {
+
+  static final Environment instance = Environment._instance();
+
+
+  factory Environment(){
+    return instance;
+  }
+
+  Environment._instance();
+
+  Future<FirebaseApp> firebaseInit() async {
+    return Firebase.initializeApp(
+      name: const String.fromEnvironment('APP_NAME'),
+      options: const FirebaseOptions(
+        apiKey: String.fromEnvironment('API_KEY'),
+        authDomain: String.fromEnvironment('AUTH_DOMAIN'),
+        projectId: String.fromEnvironment('PROJECT_ID'),
+        storageBucket: String.fromEnvironment('STORAGE_BUCKET'),
+        messagingSenderId: String.fromEnvironment('MESSAGING_SENDER_ID'),
+        appId: String.fromEnvironment('APP_ID'),
+        measurementId: String.fromEnvironment('MEASUREMENT_ID'),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:wishtag_web/core/environment.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Environment.instance.firebaseInit();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WishTag App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'WishTag App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'WishTag in dev..',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
