1cd741f82873839d67a2ba21a9f923724fe6a6ad
Oleksii Tatarintsev <semaxdev@gmail.com>
2025-02-27 22:14:51 +0200

    add error logging to pull request

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:wishtag_web/core/environment.dart';
-import 'internal/application.dart';
+//import 'internal/application.dart';
 
 final globalAuthContainer = ProviderContainer();
 
